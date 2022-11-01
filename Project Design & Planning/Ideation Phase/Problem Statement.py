acf3ba98b6069f8d690b1d1c42c7547fbae36cb8
ayyappan14 <113327133+ayyappan14@users.noreply.github.com>
2022-11-01 14:13:10 +0530

    Create Problem Statement.py

---
+++ Project Design & Planning/Ideation Phase/Problem Statement.py
@@ -0,0 +1,22 @@
+# IBM-Project-42087-1660649084
+## AI based discourse for Banking Industry - Problem Statement
+
+1. Web Application based on AI for banking system
+
+2. Developing efficient banking system based on user requirements and security, using AI
+
+3. Design and implementation of banking system for banking and financial sectors using AI
+
+4. Management information system in banking sector using AI
+
+5. Developing secured application for handling information in banking sector using AI
+
+6. USing AI, Develop smart & affordable solutions to managed secured data in banking system.
+
+7. Bank account management system using AI
+
+8. A database project on banking discourse using artificial intelligence
+
+9. Using AI, Modelling and simulation of bank management system for industrial sector
+
+10. Develop web-based solution for managing information in banking industry using AI.
